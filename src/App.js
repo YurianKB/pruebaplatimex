462127c74323d6fa8a3e62df8c5a2dfdcc972731
YurianKB <yurian@gmail.com>
2021-02-11 12:01:35 -0600

    Iniciando Create App React

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="title">
+          <p>
+            TRADE WITH THE GLOBAL FOREX
+          </p>
+          <p>
+            TRADING SPECIALIST
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
